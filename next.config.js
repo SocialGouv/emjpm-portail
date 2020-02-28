13f3ca32ed6d84d012bb3d6c45de44054a2e7840
Paul Launay <launay.p@gmail.com>
2020-02-28 19:33:05 +0100

    fix(assets): fix asset prefix issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const pages = require("./src/constants/pages.json");
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
+  assetPrefix: isProd ? "https://incubateur.social.gouv.fr/emjpm-portail" : "",
+  exportPathMap: async function() {
     const pathMap = pages.reduce((acc, page) => {
       acc[page.url] = { page: "/[...page]" };
       return acc;
