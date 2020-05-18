1e547fce1eb581ee72de5f7a84f6381b0611ceed
palimpZest <polo.vaca365@gmail.com>
2020-05-18 12:43:11 +0200

    feat(portail): refactor and clean subsections code

---
+++ src/components/ArticleSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, Flex } from "rebass";
+
+import SubPageSection from "../components/SubPageSection";
+
+const ArticleSection = ({ article, articlesLength, activeArticle, setActiveArticle }) => {
+  const selectArticle = (count) => {
+    if (count < 0) return 0;
+    if (count >= articlesLength - 1) return articlesLength - 1;
+    if (count < articlesLength) return count;
+    return;
+  };
+
+  return (
+    <SubPageSection title={article.title}>
+      <Box>{article.content}</Box>
+      <Flex
+        sx={{
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={activeArticle === 0 && { visibility: "hidden" }}>
+          <button onClick={() => setActiveArticle(selectArticle(article._id - 1))}>
+            Article précédent
+          </button>
+        </Box>
+        <Box sx={activeArticle === articlesLength - 1 && { visibility: "hidden" }}>
+          <button onClick={() => setActiveArticle(selectArticle(article._id + 1))}>
+            Article suivant
+          </button>
+        </Box>
+      </Flex>
+    </SubPageSection>
+  );
+};
+
+export default ArticleSection;
--- src/pages/professionnel/je-suis-un-mandataire-individuel/les-missions-et-les-activites-du-mandataire.js
+++ src/pages/professionnel/je-suis-un-mandataire-individuel/les-missions-et-les-activites-du-mandataire.js
@@ -1,19 +1,15 @@
-import React, { useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Box, Flex, Heading } from "rebass";
 
+import ArticleSection from "../../../components/ArticleSection";
 import SubPageSection from "../../../components/SubPageSection";
 import { mandataireMissionPageData as data } from "../../../constants/pageData";
-
-const selectArticle = (count, articleContentLength) => {
-  if (count < 0) return 0;
-  if (count >= articleContentLength) return articleContentLength - 1;
-  if (count < articleContentLength) return count;
-  return;
-};
 
 const Index = () => {
   const [active, setActive] = useState(0);
   const [activeArticle, setActiveArticle] = useState(0);
+
+  useEffect(() => () => setActiveArticle(0), [active]);
 
   return (
     <Flex bg="whiteGray">
@@ -21,67 +17,44 @@ const Index = () => {
         <Heading fontWeight="normal" mb={"50px"} mt={"28px"} fontSize={4}>
           {data.heading}
         </Heading>
-        <Box sx={{ mb: "50px" }}>
-          <Flex>
-            {data &&
-              data.sections &&
-              data.sections.map((itemButton) => (
-                <button key={itemButton._id} onClick={() => setActive(itemButton._id)}>
-                  {itemButton.title}
-                </button>
-              ))}
-          </Flex>
-        </Box>
-        <Box sx={{ my: "auto" }}>
-          {data &&
-            data.sections &&
-            data.sections.map((itemSubSection) => {
-              if (itemSubSection._id === active) {
-                return (
-                  <Box key={itemSubSection._id}>
-                    <SubPageSection title={itemSubSection.title}>
-                      {itemSubSection.content}
-                    </SubPageSection>
-                    {itemSubSection.subSection &&
-                      itemSubSection.subSection.articleContent.map((article) => {
-                        const articleContentLength =
-                          itemSubSection.subSection.articleContent.length;
-
-                        if (activeArticle === article._id) {
-                          return (
-                            <SubPageSection key={`${activeArticle}-id`} title={article.title}>
-                              <Box sx={{ whiteSpace: "pre-line" }}>{article.content}</Box>
-                              <Flex
-                                sx={{
-                                  width: "100%",
-                                }}
-                                flexWrap="wrap"
-                                justifyContent="space-between"
-                              >
-                                <button
-                                  onClick={() =>
-                                    setActiveArticle(
-                                      selectArticle(article._id - 1, articleContentLength)
-                                    )
-                                  }
-                                >{`Article précédent`}</button>
-                                <button
-                                  onClick={() =>
-                                    setActiveArticle(
-                                      selectArticle(article._id + 1, articleContentLength)
-                                    )
-                                  }
-                                >{`Article suivant`}</button>
-                              </Flex>
-                            </SubPageSection>
-                          );
-                        }
-                      })}
-                  </Box>
-                );
-              }
-            })}
-        </Box>
+        {data && data.sections && (
+          <Fragment>
+            <Box sx={{ mb: "50px" }}>
+              <Flex>
+                {data.sections.map((itemButton) => (
+                  <button key={itemButton._id} onClick={() => setActive(itemButton._id)}>
+                    {itemButton.title}
+                  </button>
+                ))}
+              </Flex>
+            </Box>
+            <Box sx={{ my: "auto" }}>
+              {data.sections.map((item) => {
+                const articles = item.subSection && item.subSection.articleContent;
+                if (item._id === active) {
+                  return (
+                    <Box key={item._id}>
+                      <SubPageSection title={item.title}>{item.content}</SubPageSection>
+                      {articles &&
+                        articles.map((article) => (
+                          <Fragment key={article._id}>
+                            {activeArticle === article._id && (
+                              <ArticleSection
+                                article={article}
+                                articlesLength={articles.length}
+                                activeArticle={activeArticle}
+                                setActiveArticle={setActiveArticle}
+                              />
+                            )}
+                          </Fragment>
+                        ))}
+                    </Box>
+                  );
+                }
+              })}
+            </Box>
+          </Fragment>
+        )}
       </Box>
     </Flex>
   );
