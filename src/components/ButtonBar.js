bbc191d33271d0ea152b50e5632862aa761ae599
palimpZest <polo.vaca365@gmail.com>
2020-05-20 17:18:58 +0200

    feat(page): add Droits du protegé page

--- src/components/ButtonBar.js
+++ src/components/ButtonBar.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Flex } from "rebass";
 
 import Button from "./common/Button";
 
 const ButtonBar = ({ data, setSectionId }) => (
-  <Flex sx={{ mb: "50px" }}>
-    {data.sections.map((itemButton) => (
-      <Button key={itemButton._id} onClick={() => setSectionId(itemButton._id)}>
-        {itemButton.title}
-      </Button>
-    ))}
-  </Flex>
+  <Fragment>
+    {data.sections && data.sections.length > 1 && (
+      <Flex sx={{ mb: "50px" }}>
+        {data.sections.map((itemButton) => (
+          <Button key={itemButton._id} onClick={() => setSectionId(itemButton._id)}>
+            {itemButton.title}
+          </Button>
+        ))}
+      </Flex>
+    )}
+  </Fragment>
 );
 
 export default ButtonBar;
--- src/content/index.js
+++ src/content/index.js
@@ -1,4 +1,18 @@
 import CuratelleContent from "./sections/CuratelleContent.md";
+import DroitsDuProtege from "./sections/DroitsDuProtege.md";
+import DroitsDuProtegeArticle1 from "./sections/DroitsDuProtegeArticle1.md";
+import DroitsDuProtegeArticle2 from "./sections/DroitsDuProtegeArticle2.md";
+import DroitsDuProtegeArticle3 from "./sections/DroitsDuProtegeArticle3.md";
+import DroitsDuProtegeArticle4 from "./sections/DroitsDuProtegeArticle4.md";
+import DroitsDuProtegeArticle5 from "./sections/DroitsDuProtegeArticle5.md";
+import DroitsDuProtegeArticle6 from "./sections/DroitsDuProtegeArticle6.md";
+import DroitsDuProtegeArticle7 from "./sections/DroitsDuProtegeArticle7.md";
+import DroitsDuProtegeArticle8 from "./sections/DroitsDuProtegeArticle8.md";
+import DroitsDuProtegeArticle9 from "./sections/DroitsDuProtegeArticle9.md";
+import DroitsDuProtegeArticle10 from "./sections/DroitsDuProtegeArticle10.md";
+import DroitsDuProtegeArticle11 from "./sections/DroitsDuProtegeArticle11.md";
+import DroitsDuProtegeArticle12 from "./sections/DroitsDuProtegeArticle12.md";
+import DroitsDuProtegeArticle13 from "./sections/DroitsDuProtegeArticle13.md";
 import JudiciaryArticleContentOne from "./sections/JudiciaryArticleContentOne.mdx";
 import JudiciaryArticleContentThree from "./sections/JudiciaryArticleContentThree.mdx";
 import JudiciaryArticleContentTwo from "./sections/JudiciaryArticleContentTwo.mdx";
@@ -14,4 +28,18 @@ export {
   JudiciaryArticleContentOne,
   JudiciaryArticleContentTwo,
   JudiciaryArticleContentThree,
+  DroitsDuProtege,
+  DroitsDuProtegeArticle1,
+  DroitsDuProtegeArticle2,
+  DroitsDuProtegeArticle3,
+  DroitsDuProtegeArticle4,
+  DroitsDuProtegeArticle5,
+  DroitsDuProtegeArticle6,
+  DroitsDuProtegeArticle7,
+  DroitsDuProtegeArticle8,
+  DroitsDuProtegeArticle9,
+  DroitsDuProtegeArticle10,
+  DroitsDuProtegeArticle11,
+  DroitsDuProtegeArticle12,
+  DroitsDuProtegeArticle13,
 };
