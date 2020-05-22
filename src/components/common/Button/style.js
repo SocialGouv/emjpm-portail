750499c8ca4ac375293d3a36c6d265afe84ae5b1
palimpZest <polo.vaca365@gmail.com>
2020-05-22 16:33:54 +0200

    fix(layout-styles): improve ButtonBar responsiveness & styling

--- src/components/common/Button/style.js
+++ src/components/common/Button/style.js
@@ -5,12 +5,23 @@ const buttonStyle = {
   fontSize: 1,
   fontWeight: 500,
   lineHeight: "1.2",
-  mx: "auto",
   outline: "none",
   position: "relative",
-  px: 3,
-  py: 2,
+  p: "10px",
+  mx: "10px",
   transition: "150ms ease-in-out opacity",
+  cursor: "pointer",
+  "@media screen and (min-width: 1200px)": {
+    ":first-of-type": {
+      ml: 0,
+    },
+    ":last-of-type": {
+      mr: 0,
+    },
+  },
+  "@media screen and (max-width: 1199px)": {
+    my: "5px",
+  },
 };
 
 export { buttonStyle };
