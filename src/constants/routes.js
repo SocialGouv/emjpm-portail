37931b274fbc0b691afc2ca3687d6dc5fea8a1fa
palimpZest <polo.vaca365@gmail.com>
2020-06-02 11:52:44 +0200

    feat(breadcrumbs): Add routes for mandataire individuel section

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,32 @@
 export const routes = {
-  "/": { label: "Accueil" },
-  "/professionnel": { label: "Professionnel" },
-  "/professionnel/je-suis-un-mandataire-individuel": { label: "Je suis un mandataire individuel" },
+  "/": { label: `Accueil` },
+  "/professionnel": { label: `Professionnel` },
+  "/professionnel/je-suis-un-mandataire-individuel": { label: `Je suis un mandataire individuel` },
+  "/professionnel/je-suis-un-mandataire-individuel/cessation-d-activite": {
+    label: `Cessation d'activité`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/demande-de-nouvel-agrement": {
+    label: `La demande de nouvel agrément`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/la-declaration-d-activite": {
+    label: `La déclaration d'activité`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/le-financement-des-mandataires-individuels": {
+    label: `Le financement des mandataires individuels`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/les-droits-du-protege": {
+    label: `Les droits du protégé`,
+  },
   "/professionnel/je-suis-un-mandataire-individuel/les-missions-et-les-activites-du-mandataire": {
-    label: "Les missions et les activités du mandataire",
+    label: `Les missions et les activités du mandataire`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/les-obligations-administratives-du-mandataire": {
+    label: `Les obligations administratives du mandataire`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/les-sanctions-en-cas-de-non-respect-des-engagements": {
+    label: `Les sanctions en cas de non-respect des engagements`,
+  },
+  "/professionnel/je-suis-un-mandataire-individuel/les-taches-du-mandataire-individuel": {
+    label: `Les tâches du mandataire individuel`,
   },
 };
