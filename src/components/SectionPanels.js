e686f54f06e355eba61952cf6bf21912f3144934
Polo Vaca Jácome <palimpZest@users.noreply.github.com>
2020-06-03 11:48:07 +0200

    feat(articles): Improve & simplify article display (#89)

--- src/components/SectionPanels.js
+++ src/components/SectionPanels.js
@@ -1,11 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Box } from "rebass";
 
-import ArticleSection from "./ArticleSection";
 import MarkdownLayout from "./MarkdownLayout";
 import SubPageSection from "./SubPageSection";
 
-const SectionPanels = ({ currentSection, articles, activeArticle, setActiveArticle }) => {
+const SectionPanels = ({ currentSection, articles }) => {
   return (
     <Box sx={{ my: "auto" }}>
       <SubPageSection title={currentSection.title}>
@@ -13,16 +12,9 @@ const SectionPanels = ({ currentSection, articles, activeArticle, setActiveArtic
       </SubPageSection>
       {articles &&
         articles.map((article) => (
-          <Fragment key={article._id}>
-            {activeArticle === article._id && (
-              <ArticleSection
-                article={article}
-                articlesLength={articles.length}
-                activeArticle={activeArticle}
-                setActiveArticle={setActiveArticle}
-              />
-            )}
-          </Fragment>
+          <SubPageSection key={article._id} title={article.title}>
+            <MarkdownLayout>{article.content}</MarkdownLayout>
+          </SubPageSection>
         ))}
     </Box>
   );
