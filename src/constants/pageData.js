6f35cde35c167a83c33de361bddaad4036096689
palimpZest <polo.vaca365@gmail.com>
2020-06-03 12:16:48 +0200

    feat(articles): Extract professionnel index page data

--- src/constants/pageData.js
+++ src/constants/pageData.js
@@ -43,32 +43,6 @@ import {
 
 const { publicRuntimeConfig } = getConfig();
 
-const mainHeadingText = `Bienvenue sur le portail de la Protection Juridique des Majeurs`;
-
-export const professionnelIndexPageData = {
-  heading: mainHeadingText,
-  sections: [
-    {
-      image: `${publicRuntimeConfig.appUrl}/bg-pro-a.png`,
-      splitTitle: { firstLine: `Je suis`, secondLine: `un mandataire individuel` },
-      title: `Je suis un mandataire individuel`,
-      url: `/professionnel/je-suis-un-mandataire-individuel`,
-    },
-    {
-      image: `${publicRuntimeConfig.appUrl}/bg-pro-c.png`,
-      splitTitle: { firstLine: `Je suis`, secondLine: `un service mandataire` },
-      title: `Je suis un service mandataire`,
-      url: `/professionnel/je-suis-un-service-mandataire`,
-    },
-    {
-      image: `${publicRuntimeConfig.appUrl}/bg-pro-b.png`,
-      splitTitle: { firstLine: `Je suis`, secondLine: `un préposé d'établissement` },
-      title: `Je suis un préposé d'établissement`,
-      url: `/professionnel/je-suis-un-prepose-d-etablissement`,
-    },
-  ],
-};
-
 export const mandataireIndividuelIndexPageData = {
   heading: `Je suis un mandataire individuel`,
   sections: [
