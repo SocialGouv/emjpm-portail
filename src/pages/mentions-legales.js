4d97ac2201d42a3316f3c174e364eef547f8a8ba
palimpZest <polo.vaca365@gmail.com>
2020-07-13 17:05:55 +0200

    feat: Add basic info to Footer

---
+++ src/pages/mentions-legales.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Flex } from "rebass";
+
+const MentionsLegales = () => (
+  <Flex
+    justifyContent="center"
+    sx={{
+      fontSize: "26px",
+      minHeight: "68vh",
+      minWidth: "50vh",
+      pt: "200px",
+    }}
+  >
+    Page en construction
+  </Flex>
+);
+
+export default MentionsLegales;
