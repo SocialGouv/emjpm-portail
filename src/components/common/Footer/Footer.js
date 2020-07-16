42c299b6ec170ce866d0fff6e9b835561851d0ea
Polo Vaca Jácome <palimpZest@users.noreply.github.com>
2020-07-16 11:11:21 +0200

    feat: Add basic info to Footer (#126)

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,7 +1,18 @@
+import getConfig from "next/config";
+import NextLink from "next/link";
 import React from "react";
-import { Box, Flex } from "rebass";
+import { Box, Flex, Link, Text } from "rebass";
 
 import Logo from "../Logo";
+import { footerItemStyle, listStyle, listTitleStyle } from "./style";
+
+const { appUrl } = getConfig().publicRuntimeConfig;
+
+const List = (props) => <Box {...props} sx={listStyle} />;
+
+const ListItem = (props) => <Text {...props} sx={listStyle} />;
+
+const ListTitle = (props) => <Text {...props} sx={listTitleStyle} />;
 
 const Footer = ({ landing }) => (
   <Box
@@ -17,6 +28,33 @@ const Footer = ({ landing }) => (
       <Box mb={[3, 0]} flexBasis={["100%", "13%"]}>
         <Logo hasTitle={false} />
       </Box>
+      <Flex flexBasis={["100%", "87%"]} justifyContent="space-evenly">
+        <List sx={footerItemStyle}>
+          <ListTitle>A propos</ListTitle>
+          <ListItem>
+            <Link target="_blank" rel="noopener noreferrer">
+              Plan de site
+            </Link>
+          </ListItem>
+          <ListItem>
+            <NextLink href={`/mentions-legales`} as={`${appUrl}/mentions-legales`} passHref>
+              <Link>Mentions légales</Link>
+            </NextLink>
+          </ListItem>
+        </List>
+        <List sx={footerItemStyle}>
+          <ListTitle>Contactez-nous</ListTitle>
+          <ListItem>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="mailto:placeholder@email.com?subject=Demande%20d%27%C3%A9volution"
+            >
+              Par email
+            </Link>
+          </ListItem>
+        </List>
+      </Flex>
     </Flex>
   </Box>
 );
