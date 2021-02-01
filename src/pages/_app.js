ac4a8b751760968aff5f924c75a727bad9b3ce75
devthejo <jo@surikat.pro>
2021-02-02 00:51:44 +0100

    feat(seo): google-site-verification

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,7 @@ function App({ Component, pageProps }) {
       <Head>
         <title>Portail de la Protection Juridique des Majeurs</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="VMN8HGADJ9JA3HTL5D28z-AqF4so6aHiBGHst3Oz69g" />
         <link
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&family=Open+Sans:ital,wght@0,600;0,700;1,400&family=Quicksand:wght@400;700&display=swap"
           rel="stylesheet"
